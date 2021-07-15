4ab3b5453d45e0d9ea2ae4f39326404ccf8ff40c
Sean Yang <hewas6@gmail.com>
2021-07-15 10:31:15 -0400

    finish scrapping

--- so.py
+++ so.py
@@ -17,27 +17,33 @@ def get_last_page():
 def extract_job(html):
     title = html.find("h2", {"class": "mb4"}).find("a")["title"]
     # print(title)
-
     company, location = html.find("h3", {"class": "fc-black-700"}).find_all(
         "span", recursive=0  # 0 = False, 1 = True
     )
     company = company.get_text(strip=1)
     location = location.get_text(strip=1)
     # print(company, location)
+    job_id = html["data-jobid"]
 
-    return {"title": title, "company": company, "location": location}
+    return {
+        "title": title,
+        "company": company,
+        "location": location,
+        "apply_link": f"https://stackoverflow.com/jobs/{job_id}",
+    }
 
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping SO: Page: {page}")
         result = requests.get(f"{URL}pg={page + 1}")
-        # print(result.status_code)
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
             # print(result["data-jobid"])
             job = extract_job(result)
+            # print(job)
             jobs.append(job)
 
     return jobs
@@ -46,10 +52,6 @@ def extract_jobs(last_page):
 def get_jobs():
     last_page = get_last_page()
     # print(last_page)
-
     jobs = extract_jobs(last_page)
 
     return jobs
-
-
-get_jobs()
