01de56183ca7ca3840a5c939f0632f8f3966a6fc
Sean Yang <hewas6@gmail.com>
2021-07-15 23:14:39 -0400

    dynamic URLs, templates

--- flask_web.py
+++ flask_web.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("FlaskScraper")
 
 
 @app.route("/")  # '@' is Decorator
 def home():
-    return "Hello, flask"
+    return render_template("basic_html.html")
+
+
+@app.route("/<username>")
+def say_name(username):
+    return f"Your name is {username}"
 
 
 @app.route("/contact")
 def contact():
-    return "Contact me!"
+    return "Contact me at hewas6@gmail.com"
 
 
 app.run(debug=True)
